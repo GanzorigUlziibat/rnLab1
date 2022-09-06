75dd714d20855bc41a4a60e4015ffce5c7b421da
GanzorigUlziibat <ganzoo@mandakh.edu.mn>
2022-09-06 13:32:57 +0800

    qwe

--- lab1/App.js
+++ lab1/App.js
@@ -1,9 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./source/screens/Home";
 import DetailScreen from "./source/screens/Detail";
+import { NativeBaseProvider } from "native-base";
 
 const Stack = createNativeStackNavigator();
 
