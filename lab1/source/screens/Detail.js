b33d57c326f02d711c5763edb2b8af64457a26c4
GanzorigUlziibat <ganzoo@mandakh.edu.mn>
2022-09-13 15:22:18 +0800

    asd

--- lab1/source/screens/Detail.js
+++ lab1/source/screens/Detail.js
@@ -1,5 +1,33 @@
 import * as React from "react";
 import { View, Text, Button } from "react-native";
+import WheelOfFortune from "react-native-wheel-of-fortune";
+
+const participants = [
+  "%10",
+  "%20",
+  "%30",
+  "%40",
+  "%50",
+  "%60",
+  "%70",
+  "%90",
+  "FREE",
+];
+const wheelOptions = {
+  rewards: participants,
+  knobSize: 50,
+  borderWidth: 5,
+  borderColor: "#000",
+  innerRadius: 50,
+  duration: 4000,
+  backgroundColor: "transparent",
+  textAngle: "horizontal",
+  // knobSource: require("../../assets/images/knob.png"),
+  getWinner: (value, index) => {
+    this.setState({ winnerValue: value, winnerIndex: index });
+  },
+  onRef: (ref) => (this.child = ref),
+};
 
 function HomeScreen({ navigation }) {
   return (
