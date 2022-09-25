87e45e3e7c139564b064155410034a50bce6d5d2
GanzorigUlziibat <ganzoo@mandakh.edu.mn>
2022-09-25 18:15:34 +0800

    qw

--- lab1/App.js
+++ lab1/App.js
@@ -4,18 +4,19 @@ import HomeScreen from "./source/screens/Home";
 import DetailScreen from "./source/screens/Detail";
 import AboutScreen from "./source/screens/About";
 import DataScreen from "./source/screens/Data";
-import { NativeBaseProvider } from "native-base";
+import ProfileScreen from "./source/screens/Profile";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Profile">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
         <Stack.Screen name="Data" component={DataScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
